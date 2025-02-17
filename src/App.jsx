1b4a0c77bf5ba2ca1bff6d98e43d71bec2ad27b9
Gabriel Urbano <g.urbanogrc@gmail.com>
2025-02-17 11:29:19 +0100

    js terminado??

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,45 @@ const DiceGame = () => {
   const [isDiceHidden, setIsDiceHidden] = useState(true);
   const [isGameOver, setIsGameOver] = useState(false);
   const [winner, setWinner] = useState(null);
-  
+
+    // Init data
+    const initData = () => {
+      setScores([0, 0]);
+      setCurrentScore(0);
+      setActivePlayer(0);
+      setIsDiceHidden(true);
+      setIsGameOver(false);
+      setWinner(null);
+    };
+
+  const handleRollDice = () => {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    setDiceValue(dice);
+    setIsDiceHidden(false);
+
+    if (dice !== 1) {
+      const newCurrentScore = currentScore + dice;
+      setCurrentScore(newCurrentScore);
+    } else {
+      setCurrentScore(0);
+      setActivePlayer(activePlayer === 0 ? 1 : 0);
+    }
+  };
+
+  const handleHold = () => {
+    const newScores = [...scores];
+    newScores[activePlayer] += currentScore;
+    setScores(newScores);
+    
+    if (newScores[activePlayer] >= 100) {
+      setIsGameOver(true);
+      setWinner(activePlayer);
+    } else {
+      setCurrentScore(0);
+      setActivePlayer(activePlayer === 0 ? 1 : 0);
+    }
+  };
+
   return (
     <main>
       {/* Componentes pig */}
