ba748ead19a6b2b4011dbb9d2e74ae7df1b943c8
Gabriel Urbano <g.urbanogrc@gmail.com>
2025-02-17 11:24:09 +0100

    estructura principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './style.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const DiceGame = () => {
+  // Varaibles
+  const [scores, setScores] = useState([0, 0]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [activePlayer, setActivePlayer] = useState(0);
+  const [diceValue, setDiceValue] = useState(5);
+  const [isDiceHidden, setIsDiceHidden] = useState(true);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [winner, setWinner] = useState(null);
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main>
+      {/* Componentes pig */}
+    </main>
+  );
+};
 
-export default App
+export default DiceGame;
